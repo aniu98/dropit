9c60a041da309baf66b81e6de7663c8720bb149e
aniu <aniu_98@163.com>
2025-01-10 19:49:23 +0800

    add

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ window.onload = function () {
                     id: 1,
                     laber: '标题1',
                 }
-            ]
+            ],
+            filesData: []
         },
         methods: {
             addHandle() {
@@ -43,14 +44,23 @@ window.onload = function () {
                 )
                 this.dialogVisible = false
             },
+            renameFiles() {
+                console.log(this.filesData);
+
+                window.renameFiles(this.filesData, ["v2025"])
+                utools.showNotification('增加时间戳完成');
+                this.clearFileNames();
+                this.outCurrentPlugin();
+            },
             dragFiles(e) {
+                console.log("sss");
                 if (e.dataTransfer && e.dataTransfer.files) {
                     for (let dragFile of e.dataTransfer.files) {
                         const file = {
                             name: dragFile.name,
                             path: dragFile.path
                         };
-                        window.checkDragFile(file);
+                        // window.checkDragFile(file);
                         if (!this.checkExistsFile(file)) {
                             this.filesData.push(file);
                         }
