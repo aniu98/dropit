42bbfcfda939b6202733f27d75d07ae3939612fc
aniu <aniu_98@163.com>
2025-01-15 22:32:06 +0800

    finish localstorag

--- assets/index.js
+++ assets/index.js
@@ -8,18 +8,7 @@ window.onload = function () {
             choseIndex: 0,
             choseNav: 1,
             content: "当前协议：",
-            filesData: [
-                {
-                    isDirectory: false,
-                    iisFile: true,
-                    name: "logo.png",
-                    path: "path",
-                    action: "$4",
-                    actionName: "重命名",
-                    destination: "",
-                    result: "tes"
-                }
-            ],
+            filesData: [],
             associations: [
                 {
                     id: '1',
@@ -31,25 +20,6 @@ window.onload = function () {
                             rules: ".*",
                             action: "$4",
                             destination: "%FileName%V%CurrentYear%%CurrentMonth%%CurrentDay%.%FileExt%"
-                        }
-                    ]
-                },
-                {
-                    id: '2',
-                    name: "oa文件移动",
-                    actived: true,
-                    associationRules: [
-                        {
-                            state: "Enabled",
-                            rules: ".*zip",
-                            action: "$1",
-                            destination: "G:\\cmft\\oa公文"
-                        },
-                        {
-                            state: "Enabled",
-                            rules: ".*",
-                            action: "$20",
-                            destination: ""
                         }
                     ]
                 }
@@ -69,33 +39,26 @@ window.onload = function () {
                 this.$el.classList.remove('drag-over');
             },
             preParse() {
-                // this.associations.forEach(e => {
-                //     if (e.actived) {
-                //         for (let i = 0; i < this.filesData.length; i++) {
-                //             matchFileAndAssociation(this.filesData[i], e.associationRules)
-                //         }
-                //     }
-                // });
                 for (let i = 0; i < this.filesData.length; i++) {
                     matchFileAndAssociation(this.filesData[i], this.associations[this.choseIndex].associationRules)
                 }
-                
+
             },
             handleDrop(e) {
                 e.preventDefault();
                 this.$el.classList.remove('drag-over');
-                
+
                 if (e.dataTransfer && e.dataTransfer.files) {
                     const dropEffect = document.createElement('div');
                     dropEffect.className = 'drop-effect';
                     dropEffect.style.left = e.clientX + 'px';
                     dropEffect.style.top = e.clientY + 'px';
                     document.body.appendChild(dropEffect);
-                    
+
                     dropEffect.addEventListener('animationend', () => {
                         document.body.removeChild(dropEffect);
                     });
-                    
+
                     for (let dragFile of e.dataTransfer.files) {
                         const file = {
                             name: dragFile.name,
@@ -117,13 +80,13 @@ window.onload = function () {
             dealFile() {
                 console.log(this.filesData);
                 this.filesData.forEach(file => {
-                    const result = window.renameFile(file);
+                    const result = autoPerformFileOperation(file);
                     // TODO 填result 不为何
                     file.result = result;
                 });
                 utools.showNotification('处理完成');
                 //TODO 如果处理失败不退出utool
-                this.outCurrentPlugin();
+                // this.outCurrentPlugin();
             },
             // 退出插件
             outCurrentPlugin() {
@@ -132,14 +95,6 @@ window.onload = function () {
             },
             handldRulesTankuang() {
                 this.showAddNav = false
-            },
-            addRules() {
-                console.log("新加规则");
-                this.showAddNav = true,
-                    this.formInfo = {
-                        name: '',
-                        id: ''
-                    }
             },
             editAssociationRules(index) {
                 this.currentEditRules = [...this.associations[this.choseIndex].associationRules];
@@ -153,23 +108,42 @@ window.onload = function () {
                 this.showEditRules = false;
                 this.preParse();
             },
-            help() {
-
+            config() {
+                
             },
+            help() { },
             choseNavBtn(id, index) {
                 this.choseNav = id;
                 this.choseIndex = index;
                 this.content = '当前协议： ' + this.associations[index].name;
                 this.preParse()
             },
-            addForm() {
-                console.log("22", this.formInfo);
+            addAssociation() {
+                console.log("新增协议");
+                this.showAddNav = true,
+                    this.associationInfo = {
+                        name: '',
+                        id: ''
+                    }
+            },
+            saveAssociation() {
+                console.log("保存协议", this.associationInfo);
                 this.associations.push(
                     {
-                        id: this.formInfo.id,
-                        name: this.formInfo.name
+                        id: this.associationInfo.id,
+                        name: this.associationInfo.name,
+                        actived: true,
+                        associationRules: [
+                            {
+                                state: "Enabled",
+                                rules: ".*",
+                                action: "$4",
+                                destination: "%FileName%V%CurrentYear%%CurrentMonth%%CurrentDay%.%FileExt%"
+                            }
+                        ]
                     }
                 )
+                utools.dbStorage.setItem("dropitConfig", this.associations);
                 this.showAddNav = false
             },
 
@@ -189,12 +163,12 @@ window.onload = function () {
                     destination: ""
                 });
             },
-            
+
             // 删除规则
             deleteRule(index) {
                 this.currentEditRules.splice(index, 1);
             },
-            
+
             // 上移规则
             moveRuleUp(index) {
                 if (index > 0) {
@@ -203,7 +177,7 @@ window.onload = function () {
                     this.currentEditRules[index - 1] = temp;
                 }
             },
-            
+
             // 下移规则
             moveRuleDown(index) {
                 if (index < this.currentEditRules.length - 1) {
@@ -212,14 +186,14 @@ window.onload = function () {
                     this.currentEditRules[index + 1] = temp;
                 }
             },
-            
+
             // 添加新方法：创建文件添加动画
             createFileAddEffect(file) {
                 const fileRow = document.createElement('div');
                 fileRow.className = 'file-add-effect';
                 fileRow.textContent = file.name;
                 document.querySelector('.right').appendChild(fileRow);
-                
+
                 // 动画结束后移除元素
                 fileRow.addEventListener('animationend', () => {
                     fileRow.remove();
@@ -230,14 +204,23 @@ window.onload = function () {
             // TODO 主题
             // document.documentElement.className = utools.isDarkColors() ? 'dark' : ''
             utools.onPluginEnter(({ code, type, payload, optional }) => {
-                console.log('用户进入插件', code, type, payload)
+                console.log('用户进入插件', code, type, payload);
+                // utools.dbStorage.setItem("aniu", "10000");
                 this.filesData = [];
                 if (type === "files") {
                     this.filesData = payload || []
                     this.preParse()
                 }
+                const value = utools.dbStorage.getItem("dropitConfig");
+                console.log("dbStorage`s valus is ", value);
+                if (!value) {
+                    console.log("true", this.associations);
+                    utools.dbStorage.setItem("dropitConfig", this.associations);
+                } else {
+                    this.associations = value;
+                    console.log("init asso", this.associations);
+                }
             });
-            this.currentFolderPath = utools.getCurrentFolderPath()
         }
     });
 }
